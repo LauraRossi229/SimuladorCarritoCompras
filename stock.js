5b61a6a82f1b46b537a71f3ff7808987b8bc593b
Laura Rossi <rossil229@gmail.com>
2023-04-01 15:27:43 -0300

    Simulador de carrito, lista productos, ordena, agrega y elimina productos

---
+++ stock.js
@@ -0,0 +1,68 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Shampoo Dove",
+        tipo: "Shampoo_Dove",
+        descripcion: "Shampoo reconstrucción completa ",
+        precio: 1200,
+        marca: "dove",
+        peso: "400 ml",
+        cantidad:0,
+    },
+
+    {
+        id: 2,
+        nombre: "Acondicionador Dove",
+        tipo: "acondicionador_Dove",
+        descripcion: "Acondicionador reconstrución completa",
+        precio: 1000,
+        marca: "dove",
+        peso: "400 ml",
+        cantidad:0,
+    },
+
+    {
+        id: 3,
+        nombre: "Crema de peinar Dove",
+        tipo: "crema_Dove",
+        descripcion: "crema de peinar dove cabellos secos",
+        precio: 1250,
+        marca: "dove",
+        peso: "200 ml",
+        cantidad:0,
+    },
+
+    {
+        id: 4,
+        nombre: "Shampoo Elvive",
+        tipo: "shampoo_Elvive",
+        descripcion: "Shampoo Elvive cabellos secos",
+        precio: 1400,
+        marca: "elvive",
+        peso: "400 ml",
+        cantidad:0,
+    },
+
+    {
+        id: 5,
+        nombre: "Acondicionador Elvive",
+        tipo: "acondicionador_Elvive",
+        descripcion: "Acondicionador Elvive cabellos teñidos",
+        precio: 1400,
+        marca: "Elvive",
+        peso: "400 ml",
+        cantidad:0,
+    },
+
+    {
+        id: 6,
+        nombre: "Crema de peinar Elvive",
+        tipo: "crema_Elvive",
+        descripcion: "crema de peinar Elvive cabellos rizados",
+        precio: 1800,
+        marca: "Elvive",
+        peso: "200 ml",
+        cantidad:0,
+    }
+]
